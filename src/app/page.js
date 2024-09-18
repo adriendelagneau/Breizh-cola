7e38185c3a6bef464b6f0e405d72044f72aee627
adriendelagneau <delagneauadrien@yahoo.fr>
2024-09-18 21:55:41 +0200

    products

--- src/app/page.js
+++ src/app/page.js
@@ -15,6 +15,7 @@ export default function Home() {
       <Landing />
       <Infos />
       <Gallery />
+   
       <SponsortsMusic />
       <Marquee />
       <Screen /> 
--- src/components/section/Infos.jsx
+++ src/components/section/Infos.jsx
@@ -76,9 +76,9 @@ const Infos = () => {
   return (
     
       
-    <div ref={container} className="flex flex-col items-center justify-center w-full gap-12 p-10 mx-auto text-xl font-extrabold dark:text-secondDarkColor text-secondColor sm:text-2xl lg:text-5xl font-poppins max-w-screen-2xl ">
+    <div ref={container} className="flex flex-col items-center justify-center w-full gap-12 p-10 mx-auto my-12 text-xl font-extrabold dark:text-secondDarkColor text-secondColor sm:text-2xl lg:text-5xl font-poppins max-w-screen-2xl">
       {infosText.map((phrase, index) => (
-        <div key={index} className="flex flex-wrap gap-2 mb-6">
+        <div key={index} className="flex flex-wrap gap-2 my-6 leading-[3.25rem]">
           {splitWords(phrase, refs)}
         </div>
       ))}
