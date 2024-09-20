afca643ba5d03b6b2b37989cc27aa57443060807
adriendelagneau <delagneauadrien@yahoo.fr>
2024-09-20 16:50:31 +0200

    chrono

--- src/components/TransitionLink.jsx
+++ src/components/TransitionLink.jsx
@@ -9,11 +9,11 @@ import { useTimelineStore2, useTimelineStore3, useTimelineStore4, useCurrentInde
 const TransitionLink = ({ href, children, setMenuOpen, myClass }) => {
   const router = useRouter();
   const pathname = usePathname(); // Get the current path
-  const { reverseTimeline2 } = useTimelineStore2(); // Access the reverse method from the Zustand store
-  const { reverseTimeline3 } = useTimelineStore3(); // Access the reverse method from the Zustand store
-  const { reverseTimeline4 } = useTimelineStore4(); // Access the reverse method from the Zustand store
-  const {resetCurrentIndex} = resetCurrentIndex();
 
+  const { reverseTimeline2 } = useTimelineStore2(); // Access reverse method from Zustand store
+  const { reverseTimeline3 } = useTimelineStore3(); // Access reverse method from Zustand store
+  const { reverseTimeline4 } = useTimelineStore4(); // Access reverse method from Zustand store
+  const { resetCurrentIndex } = useCurrentIndexStore(); // Access reset method for current index
 
   const animate = () => {
     if (href === pathname) {
@@ -28,17 +28,19 @@ const TransitionLink = ({ href, children, setMenuOpen, myClass }) => {
       top: "0",
       stagger: 0.24,
     }).then(() => {
-      // Step 2: Reverse the timeline
+      // Step 2: Reverse the timelines
       reverseTimeline2();
       reverseTimeline3();
       reverseTimeline4();
-      resetCurrentIndex();
 
       // Step 3: Navigate to the new route
       router.push(href);
       setMenuOpen && setMenuOpen(false);
 
-      // Step 4: Animate "back" effect after navigation
+      // Step 4: Reset current index
+      resetCurrentIndex(); // Reset the index to 0
+
+      // Step 5: Animate "back" effect after navigation
       gsap.to(["#transition0", "#transition1", "#transition2", "#transition3"], {
         top: "-100%",
         stagger: 0.24,
