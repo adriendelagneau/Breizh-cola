88f1a91f544accb54d92358743163ee6ce7f3bd8
adriendelagneau <delagneauadrien@yahoo.fr>
2024-10-03 23:50:07 +0200

    commit

--- src/components/FlipLayout.jsx
+++ src/components/FlipLayout.jsx
@@ -5,6 +5,7 @@ import { gsap } from "gsap";
 import { Flip } from "gsap/Flip";
 import { SponsortMusic } from "@/utils/data";
 import MagneticButtons from "./MagneticButtons";
+import Link from "next/link";
 
 gsap.registerPlugin(Flip);
 
@@ -15,7 +16,7 @@ const FlipLayout = () => {
   // Flip layout logic
   const flipLayout = () => {
     const state = Flip.getState(".circle", {
-      props: ["borderRadius", "width", "height"], // Track borderRadius, width, and height
+      props: ["borderRadius"], 
     });
 
     // Toggle the layout between 'grid' and 'stack'
@@ -25,12 +26,12 @@ const FlipLayout = () => {
     requestAnimationFrame(() => {
       Flip.from(state, {
         duration: 0.75,
-        absolute: true, // Ensure elements animate in place
-        scale: true, // Enable scaling effect
-        stagger: 0.1, // Stagger animations for a smoother effect
-        spin: true, // Add spin for more dramatic effect
-        zIndex: true, // Fix z-index layering issues
-        ease: "power1.out", // Smooth easing
+        absolute: true, 
+        scale: true, 
+        stagger: 0.1, 
+        spin: true, 
+        zIndex: true, 
+        ease: "power1.out", 
       });
     });
   };
@@ -43,27 +44,34 @@ const FlipLayout = () => {
       >
         <div className={layout === "grid" ? "grid grid-cols-5 gap-2" : "flex"}>
           {SponsortMusic.map((s, i) => (
-            <img
+            <Link
               key={i}
+              href={s.linkUrl}
+              target="_blank" // Opens in new tab
+              rel="noopener noreferrer" // Adds security to avoid exploitation
               className={`circle w-32 h-32 border-2 dark:border-mainColor border-secondColor ${
                 layout === "grid" ? "rounded-lg" : "rounded-full -ml-10"
-              }`}
-              src={s.src}
-              alt="Placeholder"
-            />
+              } overflow-hidden`} 
+            >
+              <img
+                className="object-cover w-full h-full" 
+                src={s.src}
+                alt="Placeholder"
+              />
+            </Link>
           ))}
         </div>
       </div>
       <div className="w-full text-center">
-        
-      <MagneticButtons>
+        <MagneticButtons>
           <button
-          onClick={flipLayout}
-          className="text-secondColor dark:text-secondDarkColor rotate-12 border-secondColor   h-[75px]  text-xl uppercase rounded-[50%] cursor-pointer w-[150px]  border-2 font-bold hover:text-mainColor   hover:bg-secondColor dark:hover:text-mainDarkColor dark:hover:bg-secondDarkColor dark:border-secondDarkColor 2xl:-rotate-6">
+            onClick={flipLayout}
+            className="text-secondColor dark:text-secondDarkColor rotate-12 border-secondColor h-[75px] text-xl uppercase rounded-[50%] cursor-pointer w-[150px] border-2 font-bold hover:text-mainColor hover:bg-secondColor dark:hover:text-mainDarkColor dark:hover:bg-secondDarkColor dark:border-secondDarkColor 2xl:-rotate-6"
+          >
             voir
           </button>
         </MagneticButtons>
-             </div>
+      </div>
     </>
   );
 };
